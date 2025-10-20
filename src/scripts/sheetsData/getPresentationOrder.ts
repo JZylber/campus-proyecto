ef16680e29dee648995b9469fcaa5456f05b4ef2
JZylber <zylberjulian@gmail.com>
2025-10-20 11:12:29 -0300

    feat: changed db

--- src/scripts/sheetsData/getPresentationOrder.ts
+++ src/scripts/sheetsData/getPresentationOrder.ts
@@ -3,8 +3,8 @@ import getSheetData from "./getSheetData";
 export default async (rotation: string, dataSheetId: string) => {
   const presentationOrder = await getSheetData({
     sheetID: dataSheetId,
-    sheetName: `Presentacion ${rotation}`,
-    query: `SELECT * `,
+    sheetName: `Presentacion`,
+    query: `SELECT * WHERE C = '${rotation}'`,
   });
   return presentationOrder.map((entry) => ({
     id: entry["N Grupo"] as number,
