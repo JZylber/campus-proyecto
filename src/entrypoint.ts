9ec1224a40086a888ccd75e70ef8c47093c25d96
JZylber <zylberjulian@gmail.com>
2025-10-22 16:23:06 -0300

    feat(alpine): data store

--- src/entrypoint.ts
+++ src/entrypoint.ts
@@ -2,13 +2,11 @@ import type { Alpine, ElementWithXAttributes } from "alpinejs";
 import pageData, { type PageData } from "./scripts/alpine/pageData";
 import studentData from "./scripts/alpine/studentData";
 import presentationDataStore from "./scripts/alpine/presentationData";
-import presentations from "./scripts/alpine/presentations";
 
 export default (Alpine: Alpine) => {
   Alpine.store("pageData", pageData());
   Alpine.store("studentData", studentData(Alpine));
   Alpine.store("presentationData", presentationDataStore(Alpine));
-  Alpine.data("presentations", presentations);
   const shadowContainer = document.querySelector("#campus-insertion");
   if (
     shadowContainer !== null &&
